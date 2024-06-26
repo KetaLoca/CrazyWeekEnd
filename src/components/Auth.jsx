fc7c55ca07d6cff03f89ff99bd31c406b0710123
KetaLoca <mianramar@gmail.com>
2024-06-26 11:46:19 +0200

    Trabajando FirebaseAuth

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -9,45 +9,49 @@ export const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSignUp = async () => {
-    try {
-      await createUserWithEmailAndPassword(auth, email, password);
-      alert("Usuario registrado");
-    } catch (error) {
-      console.error(error);
-      alert("Error al registrarse");
-    }
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        alert("Usuario autenticado correctamente");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Error al iniciar sesión");
+      });
   };
 
-  const handleSignIn = async () => {
-    try {
-      await signInWithEmailAndPassword(auth, email, password);
-      alert("Usuario autenticado");
-    } catch (error) {
-      console.error(error);
-      alert("Error al iniciar sesión");
-    }
+  const handleSignUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        alert("Usuario registrado correctamente");
+      })
+      .catch((error) => {
+        console.log(error);
+        alert("Error al crear el usuario");
+      });
   };
 
   return (
     <div>
       <h1>Autenticación</h1>
       <form>
-      <input
-        type="email"
-        placeholder="Correo electrónico"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Contraseña"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <br></br>
-      <button onClick={handleSignIn}>Iniciar sesión</button>
-      <button onClick={handleSignUp}>Registrarse</button>
+        <input
+          type="email"
+          placeholder="Correo electrónico"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Contraseña"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br></br>
+        <button onClick={handleSignIn}>Iniciar sesión</button>
+        <button onClick={handleSignUp}>Registrarse</button>
       </form>
     </div>
   );
