cfeafec0e9643cdb7ec9ca7c99dd307aaa8cefb1
KetaLoca <mianramar@gmail.com>
2024-06-26 13:22:31 +0200

    Estilando la Page404

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 export function Page404() {
   return (
     <>
-      <h1>La página no existe</h1>
+      <h1>La página web a la que intenta acceder no existe</h1>
       <img
         src="https://wallpapers-clan.com/wp-content/uploads/2024/03/nyan-cat-meme-gif-desktop-wallpaper-preview.gif"
         alt="NianCat volador para rellenar la pantalla"
