060dc38a87c14db5d4382dab4eacaf0976131a1a
KetaLoca <mianramar@gmail.com>
2024-06-26 10:43:06 +0200

    FirstCommit

---
+++ src/firebaseConfig.jsx
@@ -0,0 +1,23 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8lNP5tJYUUXK8m0b6dTg-ggvVDD5Rm38",
+  authDomain: "crazyweekend570.firebaseapp.com",
+  databaseURL:
+    "https://crazyweekend570-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "crazyweekend570",
+  storageBucket: "crazyweekend570.appspot.com",
+  messagingSenderId: "212699866488",
+  appId: "1:212699866488:web:71f5e00b900e4b00c1548d",
+  measurementId: "G-7BYGTK5WE6",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { auth, db };
