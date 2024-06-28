4605405f22a1d1413e154ec35aa005b8bca09b51
ketaloca <xesuspb@gmail.com>
2024-06-28 13:58:14 +0200

    Clase Alojamiento añadida en classes.js

---
+++ src/models/classes.js
@@ -0,0 +1,8 @@
+export class Alojamiento {
+    constructor(id, nombre, descripcion, imgURL) {
+        this.id = id
+        this.nombre = nombre
+        this.descripcion = descripcion
+        this.imgURL = imgURL
+    }
+}
