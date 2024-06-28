3d36ab9eaed0fc60f7df9b8dda2c164681d7f664
ketaloca <xesuspb@gmail.com>
2024-06-28 13:59:16 +0200

    Import de clase añadido en useFirestore.jsx

--- src/hooks/useFirestore.jsx
+++ src/hooks/useFirestore.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { db } from "../firebaseConfig";
 import { collection, getDocs } from "firebase/firestore"
+import { Alojamiento } from "../models/classes";
 
 export const useFirestore = () => {
     //const querySnapshot = await getDocs(collection(db, "alojamientos"))
