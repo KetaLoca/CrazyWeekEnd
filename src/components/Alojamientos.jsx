37b8ffe251f89ca6449525aeb01bf4f02d889424
ketaloca <xesuspb@gmail.com>
2024-06-28 13:33:17 +0200

    Empezando a configurar AlojamientosPage con componente Alojamientos y hook useFirestore

---
+++ src/components/Alojamientos.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Alojamientos() {
+    return (
+        <ul>
+            
+        </ul>
+    )
+ }
--- src/pages/AlojamientosPage.jsx
+++ src/pages/AlojamientosPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
 export function AlojamientosPage() {
-  return <h1>Aquí se mostrarán los alojamientos</h1>;
+  return (<h1>Aquí se mostrarán los alojamientos</h1>)
 }
