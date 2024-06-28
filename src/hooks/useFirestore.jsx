6475fd4a503543196e894481367fbe6b750036d6
ketaloca <xesuspb@gmail.com>
2024-06-28 13:47:57 +0200

    Firestore en proceso

--- src/hooks/useFirestore.jsx
+++ src/hooks/useFirestore.jsx
@@ -1,5 +1,8 @@
 import React from "react";
+import { db } from "../firebaseConfig";
+import { collection, getDocs } from "firebase/firestore"
 
-export const useFirestore = () => { 
-    
+export const useFirestore = () => {
+    //const querySnapshot = await getDocs(collection(db, "alojamientos"))
+
 }
