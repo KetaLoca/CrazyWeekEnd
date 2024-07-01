55cf07a8eb7a9293fe23ccc94f447e2fabb491fd
KetaLoca <mianramar@gmail.com>
2024-07-01 10:26:31 +0200

    Formulario para modificar perfil añadido

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -6,10 +6,20 @@ export function AccountPage() {
   return (
     <>
       <h1>Mi cuenta</h1>
+      
+      <img className="accountimg" src="src\assets\LogoAPP.png" alt="Logo de la aplicación" />
       <h2>
         Usuario logueado: <strong>{userEmail}</strong>
       </h2>
-      <img src="src\assets\LogoAPP.png" alt="Logo de la aplicación" />
+      <form className="perfilform">
+        <label>Nombre</label>
+        <input type="text" placeholder="Escriba su nombre" />
+        <label>Apellidos</label>
+        <input type="text" placeholder="Escriba sus apellidos" />
+        <label>Teléfono</label>
+        <input type="text" placeholder="Escriba su número" />
+        <button type="submit">Guardar</button>
+      </form>
     </>
   );
 }
