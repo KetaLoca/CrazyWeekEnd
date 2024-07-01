88576e8a535b8b07630b79fa9e7f0bedbf728f94
KetaLoca <mianramar@gmail.com>
2024-07-01 08:50:35 +0200

    Recuperación y mapeo de alojamientos desde FireStore implementado

--- src/hooks/useFirestore.jsx
+++ src/hooks/useFirestore.jsx
@@ -1,25 +1,26 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { db } from "../firebaseConfig";
-import { collection, getDocs } from "firebase/firestore"
+import { collection, getDocs } from "firebase/firestore";
 import { Alojamiento } from "../models/classes";
 
 export const useFirestore = () => {
-    const [alojamientos, setAlojamientos] = useState()
+  const [alojamientos, setAlojamientos] = useState();
 
-    useEffect(() => {
-
-        async function fetchAlojamientos() {
-            try {
-                const alojammientosColection = collection(db, "alojamientos")
-                const snapshot = await getDocs(alojammientosColection)
-                const alojamientosList = snapshot.docs.map(doc => Alojamiento.fromFirestore(doc))
-                setAlojamientos(alojamientosList)
-            }
-
-            catch (error) { console.error("Error recuperando los alojemientos", error) }
-        }
-        fetchAlojamientos()
-
-    }, [])
-    return { alojamientos }
-}
+  useEffect(() => {
+    async function fetchAlojamientos() {
+      try {
+        const alojammientosColection = collection(db, "alojamientos");
+        const snapshot = await getDocs(alojammientosColection);
+        const alojamientosList = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setAlojamientos(alojamientosList);
+      } catch (error) {
+        console.error("Error recuperando los alojemientos", error);
+      }
+    }
+    fetchAlojamientos();
+  }, []);
+  return { alojamientos };
+};
--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -1,8 +1,13 @@
-import React from "react"
-import {Auth} from "../components/Auth"
+import React from "react";
+import { Auth } from "../components/Auth";
+import { useFirestore } from "../hooks/useFirestore";
 
 export function AuthPage() {
+  const alojamientos = useFirestore();
   return (
-    <Auth/>
-  )
+    <>
+      <Auth />
+      {console.log(alojamientos)}
+    </>
+  );
 }
