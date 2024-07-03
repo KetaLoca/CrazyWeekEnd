6fc8c1912aa131417a52bd1ce1352c9988c71d6e
KetaLoca <mianramar@gmail.com>
2024-07-03 10:29:51 +0200

    date inputs añadidos

--- src/components/DetallesAlojamiento.jsx
+++ src/components/DetallesAlojamiento.jsx
@@ -7,6 +7,8 @@ export function DetallesAlojamiento() {
   const { getAlojammiento } = useFirestore();
   const [alojamiento, setAlojamiento] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
 
   useEffect(() => {
     getAlojammiento(id)
@@ -28,13 +30,40 @@ export function DetallesAlojamiento() {
     return <h1>No se ha encontrado el alojamiento</h1>;
   }
   return (
-    <div>
+    <div className="alojamiento">
       <h1>{alojamiento.nombre}</h1>
       <img
         src="https://www.ruralesdata.com/cache/alojamientos/aguirre-casa-rural/202-aguirre-casa-rural-elizondo-fachada.jpg"
         alt="Imagen casa rural"
       />
       <h2>{alojamiento.descripcion}</h2>
+
+      <div>
+        <label>
+          Fecha de inicio:
+          <input
+            type="date"
+            value={startDate}
+            onChange={(e) => {
+              setStartDate(e.target.value);
+            }}
+          />
+        </label>
+      </div>
+
+      <div>
+        <label>
+          Fecha de fin:{" "}
+          <input
+            type="date"
+            value={endDate}
+            onChange={(e) => {
+              setEndDate(e.target.value);
+            }}
+          />
+        </label>
+      </div>
+      <button>Reservar</button>
     </div>
   );
 }
