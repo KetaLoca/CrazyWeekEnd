7f59838a2558e194ad0c4692fc6c39b0238e44aa
KetaLoca <mianramar@gmail.com>
2024-07-03 08:43:42 +0200

    Error al cargar alojamientos arreglado

--- src/components/Alojamientos.jsx
+++ src/components/Alojamientos.jsx
@@ -4,53 +4,79 @@ import { Link } from "react-router-dom";
 
 export function Alojamientos() {
   const { alojamientos } = useFirestore();
-  const [filteredList, setFilteredList] = useState(alojamientos)
-  const [inputQuery, setInputQuery] = useState("")
-  const [sort, setSort] = useState(false)
-  const [error, setError] = useState("")
+  const [filteredList, setFilteredList] = useState(alojamientos);
+  const [inputQuery, setInputQuery] = useState("");
+  const [sort, setSort] = useState(false);
+  const [error, setError] = useState("");
 
-  useEffect(() => { setFilteredList(alojamientos) }, [])
+  useEffect(() => {
+    setFilteredList(alojamientos);
+  }, [alojamientos]);
 
   function handleSubmit(e) {
-    e.preventDefault()
+    e.preventDefault();
   }
 
   function handleChange(e) {
-    setInputQuery(e.target.value)
-    setFilteredList(alojamientos.filter((alojamiento) => alojamiento.nombre.toLowerCase().includes(inputQuery.toLowerCase())))
+    setInputQuery(e.target.value);
+    setFilteredList(
+      alojamientos.filter((alojamiento) =>
+        alojamiento.nombre.toLowerCase().includes(inputQuery.toLowerCase())
+      )
+    );
   }
 
-  function handleSort() { setSort(!sort) }
+  function handleSort() {
+    setSort(!sort);
+  }
 
-
-  return (<div>
-    <header className="filtros">
-      <h1>Buscador de alojamientos</h1>
-      <form className='form' onSubmit={handleSubmit}>
-        <span className="checkboxanimales"><label>Admiten animales</label>
-          <input type='checkbox' onChange={handleSort} checked={sort} /></span>
-        <input onChange={handleChange} value={inputQuery} name='query' placeholder='Barra de búsqueda' />
-        <button className='boton' type='submit'>Buscar</button>
-      </form>
-      {error && <p style={{ color: 'red' }} className='error'>{error}</p>}
-    </header>
-    <ul className="alojamientos">
-      {filteredList.length > 0 ? (
-        filteredList.map((alojamiento) => (
-          <li key={alojamiento.id}>
-            <Link to={`/alojamiento/${alojamiento.id}`} style={{ textDecoration: 'none', color: 'inherit' }}>Link</Link>
-            <img
-              src="https://www.ruralesdata.com/cache/alojamientos/aguirre-casa-rural/202-aguirre-casa-rural-elizondo-fachada.jpg"
-              alt="Imagen casa rural"
-            />
-            <h2>{alojamiento.nombre}</h2>
-            <p>{alojamiento.descripcion}</p>
-          </li>
-        ))
-      ) : (
-        <li>No hay alojamientos disponibles.</li>
-      )}
-    </ul>
-  </div>
+  return (
+    <div>
+      <header className="filtros">
+        <h1>Buscador de alojamientos</h1>
+        <form className="form" onSubmit={handleSubmit}>
+          <span className="checkboxanimales">
+            <label>Admiten animales</label>
+            <input type="checkbox" onChange={handleSort} checked={sort} />
+          </span>
+          <input
+            onChange={handleChange}
+            value={inputQuery}
+            name="query"
+            placeholder="Barra de búsqueda"
+          />
+          <button className="boton" type="submit">
+            Buscar
+          </button>
+        </form>
+        {error && (
+          <p style={{ color: "red" }} className="error">
+            {error}
+          </p>
+        )}
+      </header>
+      <ul className="alojamientos">
+        {filteredList.length > 0 ? (
+          filteredList.map((alojamiento) => (
+            <li key={alojamiento.id}>
+              <Link
+                to={`/alojamiento/${alojamiento.id}`}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                Link
+              </Link>
+              <img
+                src="https://www.ruralesdata.com/cache/alojamientos/aguirre-casa-rural/202-aguirre-casa-rural-elizondo-fachada.jpg"
+                alt="Imagen casa rural"
+              />
+              <h2>{alojamiento.nombre}</h2>
+              <p>{alojamiento.descripcion}</p>
+            </li>
+          ))
+        ) : (
+          <li>No hay alojamientos disponibles.</li>
+        )}
+      </ul>
+    </div>
   );
 }
