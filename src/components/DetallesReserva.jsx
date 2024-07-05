4c000bd109591a6bf45f5bb383301c49e903fea6
KetaLoca <mianramar@gmail.com>
2024-07-05 12:27:32 +0200

    getReserva() y getAlojamiento() funcionando perfectamente

--- src/components/DetallesReserva.jsx
+++ src/components/DetallesReserva.jsx
@@ -1,8 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useFirestore } from "../hooks/useFirestore";
 
 export function DetallesReserva() {
   const { id } = useParams();
+  const { getReserva, getAlojamiento } = useFirestore();
+  const [alojamiento, setAlojamiento] = useState();
+  const [reserva, setReserva] = useState();
+
+  useEffect(() => {
+    getReserva(id)
+      .then((result) => {
+        setReserva(result);
+        console.log(result);
+
+        getAlojamiento(result.idAlojamiento)
+          .then((result) => {
+            setAlojamiento(result);
+            console.log(result);
+          })
+          .catch((e) => {
+            alert("Error recuperando el alojamiento");
+          });
+      })
+      .catch((e) => {
+        alert("Error recuperando la reserva");
+      });
+  }, []);
 
   return <h1>Probando</h1>;
 }
