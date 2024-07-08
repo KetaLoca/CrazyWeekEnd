998656a7bfe02d3195253f79d8aa980cd9473e19
KetaLoca <mianramar@gmail.com>
2024-07-08 08:56:03 +0200

    Renderizado condicional arreglado usando LocalStorage

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -3,20 +3,23 @@ import React, { createContext, useState, useEffect } from "react";
 export const AuthContext = createContext();
 
 export function AuthContextProvider({ children }) {
-  const [isLogged, setIsLogged] = useState(false);
+  const [isLogged, setIsLogged] = useState();
   const [userEmail, setUserEmail] = useState("Aún no se ha logueado");
 
   useEffect(() => {
     const storedUser = localStorage.getItem("user");
+    const storedLog = localStorage.getItem("isLogged");
     if (storedUser) {
       setUserEmail(JSON.parse(storedUser));
+      setIsLogged(JSON.parse(storedLog));
     }
   }, []);
 
   useEffect(() => {
     if (isLogged) {
       localStorage.setItem("user", JSON.stringify(userEmail));
-    } 
+      localStorage.setItem("isLogged", JSON.stringify(isLogged));
+    }
   }, [isLogged]);
 
   return (
