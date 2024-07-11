14d78416dd5244540881c5fb2ab8fa4db074b8d5
ketaloca <xesuspb@gmail.com>
2024-07-11 21:00:36 +0200

    Estilos Page404 arreglados

--- src/pages/Page404.jsx
+++ src/pages/Page404.jsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 
 export function Page404() {
   return (
-    <>
-      <h1 className="titulo">La página web a la que quieres acceder no existe</h1>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <h1 className="titulo">La ruta a la que quieres acceder no existe</h1>
       <img
         src="https://wallpapers-clan.com/wp-content/uploads/2024/03/nyan-cat-meme-gif-desktop-wallpaper-preview.gif"
         alt="NianCat volador para rellenar la pantalla"
       />
       <Link className="link" to="/home">Volver a la página de inicio</Link>
-    </>
+    </div>
   );
 }
