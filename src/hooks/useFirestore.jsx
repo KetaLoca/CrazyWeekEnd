e0f6c3a88c92d7cc558609cebce3303241383710
KetaLoca <mianramar@gmail.com>
2024-07-11 12:33:26 +0200

    "onCascade" añadido manualmente. Al eliminar un alojamiento también se borran todas las reservas asociadas

--- src/hooks/useFirestore.jsx
+++ src/hooks/useFirestore.jsx
@@ -6,6 +6,7 @@ import {
   doc,
   getDoc,
   deleteDoc,
+  deleteDocs,
   where,
   query,
 } from "firebase/firestore";
@@ -45,6 +46,10 @@ export const useFirestore = () => {
   async function deleteAlojamiento(id) {
     const docRef = doc(db, "alojamientos", id);
     await deleteDoc(docRef);
+
+    const collectionRef = collection(db, "reservas");
+    const consulta = query(collectionRef, where("idalojamiento", "==", id));
+    await deleteDocs(consulta);
   }
 
   async function getAlojamientosByEmail(email) {
