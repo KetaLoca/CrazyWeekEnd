6f384a63230aefea676320e02d254daff09fcb84
ketaloca <xesuspb@gmail.com>
2024-07-18 09:21:16 +0200

    Estados, onChange() y validación del formulario

--- src/pages/AuthRegisterPage.jsx
+++ src/pages/AuthRegisterPage.jsx
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { auth } from "../firebaseConfig";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { useFirestore } from "../hooks/useFirestore";
 
 export function AuthRegisterPage() {
     const [email, setEmail] = useState("")
@@ -7,17 +10,38 @@ export function AuthRegisterPage() {
     const [nombre, setNombre] = useState("")
     const [apellidos, setApellidos] = useState("")
     const [telefono, setTelefono] = useState("")
+    const { addUser } = useFirestore()
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (!email || !password || !verifyPassword || !nombre || !apellidos || !telefono) {
+            alert("Debe rellenar todos los campos para poder registrarse")
+            return
+        }
+        if (password == verifyPassword) {
+            alert("Los dos campos de contraseña no coinciden")
+            return
+        }
+
+        createUserWithEmailAndPassword(auth, email, password).then(() => { }).catch((e) => {
+            console.error(e)
+            alert("Erro creando el usuario, el email podría estar en uso")
+        })
+    }
 
     return (
         <div className="authcontainer">
             <h1>Registrarse</h1>
-            <form className="loginform">
-                <input type="email" placeholder="Introduzca su correo electrónico" />
-                <input type="password" placeholder="Introduzca una contraseña" />
-                <input type="password" placeholder="Repita la contraseña" />
-                <input type="text" placeholder="Introduzca su nombre" />
-                <input type="text" placeholder="Introduzca sus apellidos" />
-                <input type="text" placeholder="Introduzca su teléfono" />
+            <form className="loginform" onSubmit={handleSubmit}>
+                <input type="email" placeholder="Introduzca su correo electrónico" value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input type="password" placeholder="Introduzca una contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <input type="password" placeholder="Repita la contraseña" value={verifyPassword} onChange={(e) => setVerifyPassword(e.target.value)} />
+                <input type="text" placeholder="Introduzca su nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+                <input type="text" placeholder="Introduzca sus apellidos" value={apellidos} onChange={(e) => setApellidos(e.target.value)} />
+                <input type="text" placeholder="Introduzca su teléfono" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
+                <button type="onSubmit" onClick={handleSubmit}>Registrar cuenta</button>
             </form>
         </div>
     )
