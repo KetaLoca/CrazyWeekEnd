defd7723d601759a21acaac3926173a1bc45ef1e
ketaloca <xesuspb@gmail.com>
2024-10-16 11:59:23 +0200

    cambio orden clases

--- src/models/classes.js
+++ src/models/classes.js
@@ -1,3 +1,12 @@
+export class User {
+  constructor(email, nombre, apellidos, telefono) {
+    this.email = email;
+    this.nombre = nombre;
+    this.apellidos = apellidos;
+    this.telefono = telefono;
+  }
+}
+
 export class Alojamiento {
   constructor(id, emailUser, nombre, descripcion, imgURL, animales, ubicacion) {
     this.id = id;
@@ -7,15 +16,6 @@ export class Alojamiento {
     this.imgURL = imgURL;
     this.animales = animales;
     this.ubicacion = ubicacion;
-  }
-}
-
-export class User {
-  constructor(email, nombre, apellidos, telefono) {
-    this.email = email;
-    this.nombre = nombre;
-    this.apellidos = apellidos;
-    this.telefono = telefono;
   }
 }
 
